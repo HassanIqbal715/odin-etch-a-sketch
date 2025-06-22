a2c1fccc1eb39a956e700515aa61f0c51b303787
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2025-06-22 15:56:24 +0500

    Added background-blur effect for popups.
    Adds a blur effect behind popups, and removes it when popup is closed.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const sizePopupBtn = document.querySelector("#size-popup-button");
 const sizePopupCloseBtn = document.querySelector("#size-popup-close");
 const inputX = document.querySelector("#input-x");
 const inputY = document.querySelector("#input-y");
+const blurElement = document.querySelector("#blur");
 let x = 16;
 let y = 16;
 
@@ -27,22 +28,32 @@ function checkInput(value) {
     return Number.isInteger(Number(value));
 }
 
+function closePopup() {
+    inputX.value = '';
+    inputY.value = '';
+    sizePopup.style.display = 'none';
+    blurElement.style.display = 'none';
+}
+
 sizeBtn.addEventListener("click", () => {
     sizePopup.style.display = 'flex';
+    blurElement.style.display = 'block';
 });
 
 sizePopupBtn.addEventListener("click", () => {
     if (checkInput(inputX.value) && checkInput(inputY.value)) {
         x = parseInt(inputX.value);
         y = parseInt(inputY.value);
-        grid.innerHTML = '';
-        createGrid(x, y);
+        if (x > 0 && y > 0) {
+            grid.innerHTML = '';
+            createGrid(x, y);
+        }
     }
-    sizePopup.style.display = 'none';
+    closePopup();
 });
 
 sizePopupCloseBtn.addEventListener("click", () => {
-    sizePopup.style.display = 'none';
+    closePopup();
 });
 
 createGrid(16, 16);
